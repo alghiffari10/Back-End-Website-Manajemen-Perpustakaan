f2bd94eda49a7f5e02808284de4d015440578db2
alghiffari10 <raihanalghiffary28@gmail.com>
2024-10-06 17:46:57 +0700

    first commit

---
+++ app/user/pages/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Versi</b> 1.1
+    </div>
+    <strong>Hak Cipta &copy; <?= date('Y'); ?>.</strong> Created by Raihan Alghiffari
+</footer>
